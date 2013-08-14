2e6877d8c97353f4927f91b9f6fa2e5f88718ce4
Brian Abelson <brianabelson@gmail.com>
2013-08-14 17:58:53 -0400

    first commit

---
+++ get_listicles.py
@@ -0,0 +1,19 @@
+import dataset
+import re
+import pandas as pd
+
+db = dataset.connect("sqlite:///listicles.db")
+table = db['listicles']
+
+re_listicle = re.compile("^([0-9]+).*", flags=re.IGNORECASE)
+
+listicles = []
+for row in table.all():
+  m = re_listicle.search(row['headline'])
+  if m is not None:
+    row['list_length'] = m.group(1)
+    listicles.append(row)
+
+df = pd.DataFrame(listicles)
+df.to_csv("data/listicles.csv")
+
