cb7f0224a2438ba928e54266f4c5c3518ad378e9
Noah <noah@noahveltman.com>
2013-08-15 17:49:52 +0100

    Now collects Twitter shares

--- noah-confirm-counts.py
+++ noah-confirm-counts.py
@@ -1,13 +1,30 @@
 import requests
 import re
 import json
+import logging
 from bs4 import BeautifulSoup
 
-# 811 in original list
+logger = logging.getLogger("progress")
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+def twitter_shares(url):
+	r = requests.get("http://urls.api.twitter.com/1/urls/count.json?url=http://www.buzzfeed.com"+url)
+	twitter_shares = json.loads(r.text)
+	if "count" in twitter_shares:
+		return twitter_shares["count"]	
+
+	logger.warn("No count available for http://www.buzzfeed.com"+url)	
+	logger.warn("Received: "+str(r.text))
+	return 0
+
 lists = json.loads(open("results-initial.json","r").read())
 results = []
 
-for l in lists:	
+for l in lists:
 
 	r = requests.get("http://www.buzzfeed.com"+l["url"])
 	if r.status_code == 200:		
@@ -25,18 +42,21 @@ for l in lists:
 		divs = contents.find_all("div",class_="buzz_superlist_item")
 
 		if len(divs) >= l["expectedListLength"]-5 and len(divs) <= l["expectedListLength"]+5:
-			results.append(l)
+			l["twitterShares"] = twitter_shares(l["url"])
+			results.append(l)						
 			continue
 
 		spans = contents.find_all("span",class_="buzz_superlist_number")
 
 		if len(spans) >= l["expectedListLength"]-5 and len(spans) <= l["expectedListLength"]+5:
+			l["twitterShares"] = twitter_shares(l["url"])
 			results.append(l)
 			continue		
 		
 		spans = contents.find_all("span",class_="buzz_superlist_number_inline")
 
 		if len(spans) >= l["expectedListLength"]-5 and len(spans) <= l["expectedListLength"]+5:
+			l["twitterShares"] = twitter_shares(l["url"])
 			results.append(l)
 			continue
 					
